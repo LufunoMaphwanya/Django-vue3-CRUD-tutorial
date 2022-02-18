07724eff65f60b226bcd9f3df76ae80025d23c57
Lufuno Maphwanya <lcmaphwanya@gmail.com>
2022-02-18 20:40:59 +0200

    Adds how component.

--- subscription-app/src/components/Show.vue
+++ subscription-app/src/components/Show.vue
@@ -1,3 +1,43 @@
 <template>
-    <h1>Show </h1>
+    <div class="card" style="width: 100%; float: left; margin: 5px" >
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Amount</th>
+                <th scope="col">#</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{subscription.name}}</td>
+                    <td>{{subscription.description}}</td>
+                    <td>{{ subscription.currency}}{{ subscription.amount}}</td>
+                </tr>
+            </tbody>
+            </table>
+    </div>
 </template>
+<script>
+import useSubscriptions from '../composables/subscriptions';
+import { onMounted } from 'vue';
+
+export default {
+    props: {
+       id: {
+           required: true,
+           type: String
+       }
+   },
+    setup(props) {
+        const { subscription, getSubscription } = useSubscriptions()
+
+        onMounted(() => getSubscription(props.id));
+
+        return {
+            subscription
+        }
+    }
+}
+</script>
